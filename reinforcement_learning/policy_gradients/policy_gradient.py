4daa9b4c923d9fdc19bbfc4098eba7dbb12948d1
spindouken <mlc9181209@gmail.com>
2024-01-12 17:06:13 -0600

    protofiles

---
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+computes to policy with a weight of a matrix
+"""
+
+
+def policy(matrix, weight):
+    """
+    
+    """
--- reinforcement_learning/temporal_difference/0-monte-carlo.py
+++ reinforcement_learning/temporal_difference/0-monte-carlo.py
@@ -17,4 +17,4 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0
 
     Returns: V, the updated value estimate
     """
-
+    
