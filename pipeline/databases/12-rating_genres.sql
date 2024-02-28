241a83de8d216ddaef2a15829da2f0000fcc46ec
spindouken <mlc9181209@gmail.com>
2024-02-28 00:14:00 -0600

    column name requirements, task 5 working

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -1,7 +1,7 @@
 -- lists all genres by their rating sum, sorted in descending order
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating_sum
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
