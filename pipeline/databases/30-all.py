d38cbceaca2758f2828df59c03c55df95fe74e91
spindouken <mlc9181209@gmail.com>
2024-02-28 22:25:38 -0600

    task 30 - list all documents in mongo collection

--- pipeline/databases/30-all.py
+++ pipeline/databases/30-all.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
-""" 30-main """
-from pymongo import MongoClient
-list_all = __import__('30-all').list_all
+"""task30"""
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {}".format(school.get('_id'), school.get('name')))
+
+def list_all(mongo_collection):
+    """list all documents in a collection"""
+    return mongo_collection.find()
