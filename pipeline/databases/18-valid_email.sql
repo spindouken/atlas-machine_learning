c0a84ca6c086583f6e743be5595a61378a497bd3
spindouken <mlc9181209@gmail.com>
2024-02-28 23:15:46 -0600

    task 18 - trigger update

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,15 @@
+-- creates a trigger that resets the attribute valid_email
+--  only when the email has been changed
+
+DELIMITER $$
+
+CREATE TRIGGER before_email_update
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email <> NEW.email THEN
+        SET NEW.valid_email = FALSE;
+    END IF;
+END$$
+
+DELIMITER ;
