ce8b565e4c5c27f24d633d226ff98d7d1e6f547b
spindouken <mlc9181209@gmail.com>
2024-02-28 22:28:40 -0600

    task 31 - mongo insert doc

---
+++ pipeline/databases/31-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""task31"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new document in a collection"""
+    return mongo_collection.insert_one(kwargs).inserted_id
